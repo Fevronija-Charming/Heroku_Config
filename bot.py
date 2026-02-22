a8a542523cc4d6fa5966b69b902e6615d183c4ac
Fevronija Rybkina <anton.tsizov@tkvg.ee>
2026-02-22 23:55:48 +0200

    вынос базы данных и более красивое представление

--- bot.py
+++ bot.py
@@ -103,88 +103,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 engine = create_async_engine(os.getenv("DBURL"),echo=True,max_overflow=5)
 session_factory = async_sessionmaker(bind=engine,class_=AsyncSession,expire_on_commit=False)
-class Base(DeclarativeBase):
-    pass
-class Platoky(Base):
-    __tablename__="ПППЛАТКИ"
-    id: Mapped[int]=mapped_column(primary_key=True, autoincrement=True, nullable=False)
-    Название: Mapped[str]=mapped_column(String(128), nullable=False)
-    Автор: Mapped[str]=mapped_column(String(128), nullable=False)
-    Колорит_1: Mapped[str]=mapped_column(String(128), nullable=False)
-    Колорит_2: Mapped[str] = mapped_column(String(128), nullable=False)
-    Колорит_3: Mapped[str] = mapped_column(String(128), nullable=False)
-    Колорит_4: Mapped[str] = mapped_column(String(128), nullable=False)
-    Колорит_5: Mapped[str] = mapped_column(String(128), nullable=False)
-    Узор_темени: Mapped[str] = mapped_column(String(128), nullable=False)
-    Узор_сердцевины: Mapped[str] = mapped_column(String(128), nullable=False)
-    Узор_сторон: Mapped[str] = mapped_column(String(128), nullable=False)
-    Узор_углов: Mapped[str] = mapped_column(String(128), nullable=False)
-    Узор_края: Mapped[str] = mapped_column(String(128), nullable=False)
-    Цветы_Орнамент: Mapped[str] = mapped_column(String(128), nullable=False)
-    Изображенный_Цветок_1: Mapped[str] = mapped_column(String(128), nullable=False)
-    Изображенный_Цветок_2: Mapped[str] = mapped_column(String(128), nullable=False)
-    Изображенный_Цветок_3: Mapped[str] = mapped_column(String(128), nullable=False)
-    Изображенный_Цветок_4: Mapped[str] = mapped_column(String(128), nullable=False)
-    Изображенный_Цветок_5: Mapped[str] = mapped_column(String(128), nullable=False)
-    Размер_Платка: Mapped[str]=mapped_column(String(128), nullable=False)
-    Материал_Платка: Mapped[str]=mapped_column(String(128), nullable=False)
-    Материал_Бахромы: Mapped[str]=mapped_column(String(128), nullable=False)
-    # для проверки '''INSERT INTO ПППЛАТКИ (id, Название, Автор, Колорит_1, Колорит_2, Колорит_3,
-    # Колорит_4, Колорит_5, Узор_темени, Узор_сердцевины, Узор_сторон, Узор_углов, Узор_края,
-    # Цветы_Орнамент, Изображенный_Цветок_1, Изображенный_Цветок_2, Изображенный_Цветок_3,
-    # Изображенный_Цветок_4, Изображенный_Цветок_5, Размер_Платка, Материал_Платка, Материал_Бахромы)'''
-class Tradicii(Base):
-    __tablename__="ТРАДИЦИИ_НОШЕНИЯ"
-    id: Mapped[int]=mapped_column(primary_key=True, nullable=False)
-    Название_традиции_1: Mapped[str]=mapped_column(String(128), nullable=False)
-    Название_традиции_2: Mapped[str]=mapped_column(String(128), nullable=False)
-    Название_традиции_3: Mapped[str]=mapped_column(String(128), nullable=False)
-    Фото_ссылка_1: Mapped[str] = mapped_column(String(128), nullable=False)
-    Фото_ссылка_2: Mapped[str] = mapped_column(String(128), nullable=False)
-    Фото_ссылка_3: Mapped[str] = mapped_column(String(128), nullable=False)
-    Фото_ссылка_4: Mapped[str] = mapped_column(String(128), nullable=False)
-    Фото_ссылка_5: Mapped[str] = mapped_column(String(128), nullable=False)
-    Описание_этническое: Mapped[str] = mapped_column(Text, nullable=False)
-    Описание_техническое: Mapped[str] = mapped_column(Text, nullable=False)
-    Видео_ссылка_1: Mapped[str] = mapped_column(String(128), nullable=False)
-    Видео_ссылка_2: Mapped[str] = mapped_column(String(128), nullable=False)
-    Сфера_применения_1: Mapped[str] = mapped_column(String(128), nullable=False)
-    Сфера_применения_2: Mapped[str] = mapped_column(String(128), nullable=False)
-    Форма_платка_при_одевании_1: Mapped[str] = mapped_column(String(128), nullable=False)
-    Форма_платка_при_одевании_2: Mapped[str] = mapped_column(String(128), nullable=False)
-    Способ_закрепления_платка_1: Mapped[str] = mapped_column(String(128), nullable=False)
-    Способ_закрепления_платка_2: Mapped[str] = mapped_column(String(128), nullable=False)
-    Этническая_классификация: Mapped[str] = mapped_column(String(128), nullable=False)
-    Географическое_распространение: Mapped[str] = mapped_column(Text, nullable=False)
-class Banda(Base):
-    __tablename__="Платочная_Банда"
-    id: Mapped[int]=mapped_column(primary_key=True, autoincrement=True, nullable=False)
-    Гражданское_Имя: Mapped[str]=mapped_column(String(128), nullable=False)
-    Творческий_Псевдоним: Mapped[str]=mapped_column(String(128), nullable=False)
-    Описание_Творческой_Деятельности: Mapped[str]=mapped_column(Text, nullable=False)
-    Связь_Творчества_С_Павлопосадскими_Платками: Mapped[str] = mapped_column(Text, nullable=False)
-    Ссылка_На_Инстаграм: Mapped[str] = mapped_column(String(128), nullable=False)
-    Ссылка_На_ВК: Mapped[str] = mapped_column(String(128), nullable=False)
-    Ссылка_На_Ютуб: Mapped[str] = mapped_column(String(128), nullable=False)
-    Ссылка_На_Фейсбук: Mapped[str] = mapped_column(String(128), nullable=False)
-    Ссылка_На_Телеграм: Mapped[str] = mapped_column(String(128), nullable=False)
-    Ссылка_На_Одноклассники: Mapped[str] = mapped_column(String(128), nullable=False)
-    Ссылка_На_Яндекс_Дзен: Mapped[str] = mapped_column(String(128), nullable=False)
-    Ссылка_На_Сайт: Mapped[str] = mapped_column(String(128), nullable=False)
-    Адрес_Деятельности: Mapped[str] = mapped_column(String(128), nullable=False)
-class Symboly(Base):
-    __tablename__="Значение_Символов_Орнамента"
-    id: Mapped[int]=mapped_column(primary_key=True, autoincrement=True, nullable=False)
-    Название_Символа: Mapped[str]=mapped_column(String(32), nullable=False)
-    Значение_Символа: Mapped[str]=mapped_column(Text, nullable=False)
-    Встречается_На_Платках: Mapped[str]=mapped_column(Text, nullable=False)
-    Ассоциативная_Иллюстрация_1: Mapped[str] = mapped_column(String(128), nullable=False)
-    Ассоциативная_Иллюстрация_2: Mapped[str] = mapped_column(String(128), nullable=False)
-    Символ_На_Платке_1: Mapped[str] = mapped_column(String(128), nullable=False)
-    Символ_На_Платке_2: Mapped[str] = mapped_column(String(128), nullable=False)
-    Символ_На_Платке_3: Mapped[str] = mapped_column(String(128), nullable=False)
-    Символ_На_Платке_4: Mapped[str] = mapped_column(String(128), nullable=False)
-    Символ_На_Платке_5: Mapped[str] = mapped_column(String(128), nullable=False)
+from datamodels import Platoky,Tradicii,Banda,Symboly
 async def create_platky():
     async with engine.begin() as connection:
         await connection.run_sync(Base.metadata.create_all)
@@ -216,8 +135,9 @@ dp=Dispatcher()
 dp.message.middleware(AdminControl(Bot))
 dp.message.middleware(RezimRabotyAdmina(Bot))
 # клавы из файла классов
-from klaviatury import klava_poisk1, klava_poisk2, klava_kolorit, klava_posobiy, klava_material, klava_bahroma
+from klaviatury import klava_poisk1, klava_poisk2, klava_kolorit, klava_posobiy, klava_material, klava_bahroma,klava_symboly
 from klaviatury import klava_admina_uroki, klava_admina_platki, klava_admina_glav,klava_admina_materialy, klava_privetstvije
+from templates import platok_predstav
 # костыль для получения id фото в системе тг
 class Vvod_Foto(StatesGroup):
     get_foto_id=State()
@@ -454,6 +374,10 @@ async def proverka_tradicii(message: types.Message):
     else:
         await message.answer(text="Нечего проверять, сначала введите запись в буфер")
 # логика основных команд по ключевым словам
+@dp.message((F.text.lower() == "значение символов на платке"))
+@dp.message((F.text.lower()=="/poisk_autora"))
+async def otrisovka_symbola1(message: types.Message):
+    await message.answer(text="Выбран сегмент символов на платке", reply_markup=ReplyKeyboardRemove())
 @dp.message(or_f((Command("glav_poisk")),(F.text.lower()=="поиск по техническим харак."),(F.text.lower()=="основной")))
 async def poisk1(message: types.Message):
     await message.answer(text="Поиск1",reply_markup=klava_poisk1)
@@ -870,8 +794,8 @@ async def vvod_nazvanija_platka(message: types.Message):
         while True:
             next_row=cursor.fetchone()
             if next_row:
-                print(f"{next_row}")
-                await message.answer(text=f"{next_row}")
+                for element, segment in next_row, platok_predstav:
+                    await message.answer(text=f"{segment}{element}")
             else:
                 break
         # синхронизация изменений, комит версии
